2ec814fda8bd4360a9e4796f2036a98a858d07a5
Clemmie <clemmie@imanagesystems.com>
2021-03-11 17:30:30 +0800

    Initial Commit

---
+++ LJStepView/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LJStepView
+//
+//  Created by IMS_Mac on 2021/3/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LJStepView/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  LJStepView
+//
+//  Created by IMS_Mac on 2021/3/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
